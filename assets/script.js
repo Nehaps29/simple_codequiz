c02e5fd052ae09c5565e206acdcbf793164c4de6
Nehaps29 <133929172+Nehaps29@users.noreply.github.com>
2023-07-24 09:47:37 -0400

    added initial html, css, js files

---
+++ assets/script.js
@@ -0,0 +1,10 @@
+var startButton = document.querySelector("#start_button");
+var firstPage = document.querySelector(".first_page");
+
+startButton.addEventListener("click", startQuiz);
+
+
+function startQuiz() {
+    firstPage.display = none;
+    var read = "start is working";
+}
